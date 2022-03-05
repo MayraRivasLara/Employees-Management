84f4ba7448df34c39140549bbcdea9501b1db75b
Mayra Rivas Lara <mayrulara@hotmail.com>
2022-03-05 19:51:16 +0800

    js files done

---
+++ src/departments.js
@@ -0,0 +1,27 @@
+// CRUD - Create Read Update and Delete
+
+/**
+ * Create a new department in db.
+ * @param {string} name 
+ */
+function createDepartment(name){
+    
+}
+
+/**
+ * Getting all the departments from db
+ */
+function getDepartments(){
+
+}
+
+// for the future implementation 
+//function updateDepartments(id, salary){
+
+// };
+
+
+module.exports= {
+    createDepartment,
+    getDepartments,
+}
---
+++ src/employees.js
@@ -0,0 +1,28 @@
+// CRUD - Create Read Update and Delete
+
+/**
+ * Create a new employee in db.
+ * @param {string} firstName 
+ * @param {string} lastName 
+ * @param {number} roleId 
+ * @param {number} managerId 
+ */
+ function createEmployee(firstName, lastName, roleId, managerId){
+    
+}
+
+/**
+ * Getting all the employees from db
+ */
+function getEmployees(){
+
+}
+
+/**
+ * 
+ * @param {number} employeeId 
+ * @param {number} newRole 
+ */
+function updateEmployeeRole(employeeId, newRoleId){
+
+};
